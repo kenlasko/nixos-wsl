8def0359e746d5e1fbe1dd20656c0d1121c1d6db
kenlasko <klasko@nectarcorp.com>
2025-03-25 17:38:21 -0400

    Update README and copy-config.sh script for improved configuration copying

--- scripts/copy-config.sh
+++ scripts/copy-config.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/sh
 
 # Get the WSL host path for the logged-in user
 HOSTPATH=$(echo "$WSLPATH" | grep -oP '(/mnt/\w/Users/\w+)' | head -1)
@@ -7,29 +7,33 @@ HOSTPATH=$(echo "$WSLPATH" | grep -oP '(/mnt/\w/Users/\w+)' | head -1)
 if [ -f "$HOSTPATH/.kube/config" ]; then
     [ ! -d ~/.kube ] && mkdir -p ~/.kube 
     cp "$HOSTPATH/.kube/config" ~/.kube/config
+    echo "Copied kubeconfig file from $HOSTPATH"
 else
     echo "No kubeconfig file found in $HOSTPATH"
 fi
 
 # Get talosctl and omnictl config files from the Omni repo
-if [ -f "~/omni/talosconfig.yaml" ]; then
+if [ -f ~/omni/talosconfig.yaml ]; then
     [ ! -d ~/.talos ] && mkdir -p ~/.talos 
-    cp "~/omni/talosconfig.yaml" ~/.talos/config
+    cp ~/omni/talosconfig.yaml ~/.talos/config
+    echo "Copied talosconfig file from ~/omni"
 else
     echo "No talosconfig file found. Clone the kenlasko/omni repo to get this file"
 fi
 
-if [ -f "~/omni/omniconfig.yaml" ]; then
+if [ -f ~/omni/omniconfig.yaml ]; then
     [ ! -d ~/.config/omni/ ] && mkdir -p ~/.config/omni/
-    cp "~/omni/omniconfig.yaml" ~/.config/omni/config
+    cp ~/omni/omniconfig.yaml ~/.config/omni/config
+    echo "Copied omniconfig file from ~/omni"
 else
     echo "No omniconfig file found. Clone the kenlasko/omni repo to get this file"
 fi
 
 # Get the Ansible hosts file from the k8s repo
-if [ -f "~/k8s/ansible/hosts" ]; then
+if [ -f ~/k8s/ansible/resources/hosts ]; then
     [ ! -d /etc/ansible ] && sudo mkdir -p /etc/ansible
-    sudo cp "~/k8s/ansible/hosts" /etc/ansible/hosts
+    sudo cp ~/k8s/ansible/resources/hosts /etc/ansible/hosts
+    echo "Copied Ansible hosts file from ~/k8s"
 else
-    echo "No hosts file found. Clone the kenlasko/k8s repo to get this file"
+    echo "No Ansible hosts file found. Clone the kenlasko/k8s repo to get this file"
 fi
