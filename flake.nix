dbf57dde7581f937dd5c98291343064d0ce5fa69
Ken Lasko <ken.lasko@gmail.com>
2025-03-25 22:32:19 -0400

    Fix nix-ld package reference in flake.nix for correct sourcing

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
           {
             programs.nix-ld = {
               enable = true;
-              package = nixpkgs.nix-ld-rs;
+              package = pkgs.nix-ld-rs;
             };
           }
         ];
