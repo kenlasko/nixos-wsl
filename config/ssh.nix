1e83b5b085adee8a15d06b404c79c47e92596e8e
Ken Lasko <ken.lasko@gmail.com>
2025-03-26 22:29:33 -0400

    Remove deprecated SSH key decryption configuration from SOPS setup

--- config/ssh.nix
+++ config/ssh.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
-let
-  # Decrypt the SSH key using sops-nix
-  decryptedKey = config.sops.secrets."secrets/id_rsa.sops.yaml".text;
-in
+# let
+#   # Decrypt the SSH key using sops-nix
+#   decryptedKey = config.sops.secrets."secrets/id_rsa.sops.yaml".text;
+# in
 {
   # Enable the SSH agent
 }
