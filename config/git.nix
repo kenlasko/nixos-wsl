07dfa299723dcb19a5a0dc77f40cb555ad6307e5
Ken Lasko <ken.lasko@gmail.com>
2025-04-24 18:00:07 -0400

    Testing new commit

--- config/git.nix
+++ config/git.nix
@@ -3,7 +3,7 @@
 }: {
   home-manager.users.ken = {
     home.packages = with pkgs; [
- 
+      pinentry
     ];
 
 
@@ -14,19 +14,28 @@
       userEmail = "ken.lasko@gmail.com";
 
       extraConfig = {
+
         init.defaultBranch = "main";
         url = {
           "ssh://git@github.com" = {
             insteadOf = "https://github.com";
           };
         };
+        # Add GPG signing configurations
+        user.signingkey = "3A82EFDA2CD08F09";
+        commit.gpgsign = true;
+        gpg.program = "${pkgs.gnupg}/bin/gpg";
       };
     };
 
-    # GnuPG
-    programs.gnupg.agent = {
+    # GPG Configuration
+    programs.gpg = {
       enable = true;
-      enableSSHSupport = true;
+    };
+    # Enable GPG Agent via services
+    services.gpg-agent = {
+      enable = true;
+      pinentryPackage = pkgs.pinentry;
     };
   };
 }
