2e3815b5d1eb785c9f39c1caf1f0dd62f7f770a1
Ken Lasko <ken.lasko@gmail.com>
2025-07-16 21:37:19 -0400

    Remove Ansible hosts file retrieval from cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -15,15 +15,6 @@ echo "Symlink `/etc/nixos` to Github synced folder."
 sudo rm -rf /etc/nixos/
 sudo ln -s ~/nixos /etc/nixos
 
-# Get the Ansible hosts file from the k8s repo
-if [ -f ~/k8s/ansible/resources/hosts ]; then
-    [ ! -d /etc/ansible ] && sudo mkdir -p /etc/ansible
-    sudo cp ~/k8s/ansible/resources/hosts /etc/ansible/hosts
-    echo "Copied Ansible hosts file from ~/k8s"
-else
-    echo "No Ansible hosts file found. Clone the kenlasko/k8s repo to get this file"
-fi
-
 # Delete the no longer needed nixos home directory
 echo "Deleting /home/nixos directory"
 sudo rm -rf /home/nixos
