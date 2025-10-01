ddd111db4029a0f7430b310dccd26e720307a3c2
Ken Lasko <ken.lasko@gmail.com>
2025-10-01 08:05:03 -0400

    Add Docker login commands

--- config/docker.nix
+++ config/docker.nix
@@ -8,4 +8,18 @@
       docker-buildx
     ];
   };
+
+  # Auto-login to Docker Hub using /run/secrets/docker_pat
+  systemd.services.docker-login = {
+    description = "Docker Auto Login";
+    after = [ "docker.service" ];
+    wants = [ "docker.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        /bin/sh -c 'cat /run/secrets/docker_pat | ${pkgs.docker}/bin/docker login -u kenlasko --password-stdin'
+      '';
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 }
